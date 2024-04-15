0c268ad1f15f598a875bd8ed3874930a4d6da713
JoaoMNZ <joao.menezes21@Outlook.com>
2024-04-15 19:24:50 -0300

    php

--- teste.php
+++ teste.php
@@ -1,27 +1,45 @@
 <?php
-// Verifica se os campos foram preenchidos
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['nome']) && !empty($_POST['email'])) {
-    // Informações do remetente
+// Verifica se os dados foram enviados por POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Captura os dados do formulário
+    $tipo = $_POST['tipo'];
+    $produto = $_POST['produto'];
     $nome = $_POST['nome'];
-    $email_cliente = $_POST['email'];
+    $sobrenome = $_POST['sobrenome'];
+    $cpf = $_POST['cpf'];
+    $email = $_POST['email'];
+    $cep = $_POST['cep'];
+    $numero = $_POST['numero'];
+    $logradouro = $_POST['logradouro'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
     
-    // E-mail de destino
-    $meu_email = "seu_email@example.com"; // Insira aqui o seu e-mail
-    
-    // Assunto do e-mail
-    $assunto = "Novo contato do site";
+    // Formata a mensagem a ser enviada por e-mail
+    $mensagem = "Tipo: $tipo\n";
+    $mensagem .= "Produto: $produto\n";
+    $mensagem .= "Nome: $nome\n";
+    $mensagem .= "Sobrenome: $sobrenome\n";
+    $mensagem .= "CPF: $cpf\n";
+    $mensagem .= "Email: $email\n";
+    $mensagem .= "CEP: $cep\n";
+    $mensagem .= "Número: $numero\n";
+    $mensagem .= "Logradouro: $logradouro\n";
+    $mensagem .= "Bairro: $bairro\n";
+    $mensagem .= "Cidade: $cidade\n";
+    $mensagem .= "Estado: $estado\n";
 
-    // Mensagem a ser enviada
-    $mensagem = "Nome: " . $nome . "\n";
-    $mensagem .= "Email: " . $email_cliente . "\n";
+    // Email de destino
+    $meu_email = "joao.menezes21@outlook.com"; // Insira aqui o seu e-mail
+    $assunto = "Novo pedido de orçamento";
 
     // Envia o e-mail
     if (mail($meu_email, $assunto, $mensagem)) {
-        echo "Obrigado por entrar em contato, $nome. Seu email foi enviado com sucesso!";
+        echo "Obrigado por solicitar um orçamento, $nome. Seu pedido foi enviado com sucesso!";
     } else {
-        echo "Desculpe, ocorreu um erro ao enviar seu email.";
+        echo "Desculpe, ocorreu um erro ao processar o seu pedido.";
     }
 } else {
-    echo "Por favor, preencha todos os campos do formulário.";
+    echo "Por favor, preencha o formulário corretamente.";
 }
 ?>
